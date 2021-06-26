5109266ea44b8b09978009426dac1df942e22c82
Justin Coulston <justincoulston92@gmail.com>
2021-06-26 10:07:16 +0100

    test prod build with script

--- pre-build.js
+++ pre-build.js
@@ -1,7 +1,43 @@
 const fs = require('fs');
+const fse = require('fs-extra');
 
-fs.readdir('./packages/app/.next', (err, files) => {
-  files.forEach((file) => {
-    console.log(file);
-  });
-});
+function init() {
+  const srcDir = `./packages/app/.next`;
+  const destDir = `./.next`;
+
+  //   // fs.readdir('./packages/app/.next', (err, files) => {
+  //   //   files.forEach((file) => {
+  //   //     console.log(file);
+  //   //   });
+  //   // });
+
+  //   // To copy a folder or file
+  //   fse.copySync(srcDir, destDir, (err) => {
+  //     console.log('innit copy');
+  //     if (err) {
+  //       console.error('ERR', err);
+  //     } else {
+  //       console.log('success! file copy');
+  //     }
+  //   });
+  // }
+
+  const { promises: fs } = require('fs');
+  const path = require('path');
+
+  async function copyDir(src, dest) {
+    await fs.mkdir(dest, { recursive: true });
+    let entries = await fs.readdir(src, { withFileTypes: true });
+
+    for (let entry of entries) {
+      let srcPath = path.join(src, entry.name);
+      let destPath = path.join(dest, entry.name);
+
+      entry.isDirectory() ? await copyDir(srcPath, destPath) : await fs.copyFile(srcPath, destPath);
+    }
+  }
+
+  copyDir(srcDir, destDir);
+}
+
+init();
